50b52a1f59ce9d7b9b0fd5b01310a6ef80130d16
Botir <tiboruz@gmail.com>
2026-01-26 15:50:27 +0500

    sdfsadfsadfsadfa

--- resources/views/components/label-size.blade.php
+++ resources/views/components/label-size.blade.php
@@ -16,7 +16,7 @@
     <text x="{{ $w/2 }}" y="{{ $h/2 }}"
           text-anchor="middle"
           dominant-baseline="middle"
-          font-size="12px"
+          font-size="1em"
           font-weight="700"
           fill="grey">
         {{ $w }} × {{ $h }} mm
